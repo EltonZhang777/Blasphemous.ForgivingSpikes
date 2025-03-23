7db8844068a26991b8a302de9699d9cb34508ad5
EltonZhang777 <280903158@qq.com>
2025-03-23 09:39:16 +0800

    Initial commit

---
+++ Blasphemous.ForgivingSpikes/ForgivingSpikes.cs
@@ -0,0 +1,14 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.ForgivingSpikes
+{
+    public class ForgivingSpikes : BlasMod
+    {
+        public ForgivingSpikes() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+        protected override void OnInitialize()
+        {
+            LogError($"{ModInfo.MOD_NAME} has been initialized");
+        }
+    }
+}
---
+++ Blasphemous.ForgivingSpikes/Main.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace Blasphemous.ForgivingSpikes
+{
+    [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+    [BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public static ForgivingSpikes ForgivingSpikes { get; private set; }
+
+        private void Start()
+        {
+            ForgivingSpikes = new ForgivingSpikes();
+        }
+    }
+}
